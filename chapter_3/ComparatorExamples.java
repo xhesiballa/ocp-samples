9e06f64382b8a3aabf320e54a864e01dce75ac47
Xhesi Balla <xhesiballa15@gmail.com>
2018-09-12 16:09:54 +0200

    Java 8 multi properties comparator

--- chapter_3/ComparatorExamples.java
+++ chapter_3/ComparatorExamples.java
@@ -11,11 +11,22 @@ public class ComparatorExamples {
 			}
 		};
 
-		Product rubber = new Product(3, "Rubber");
-		Product pen = new Product(1, "Pen");
-		Product noteBook = new Product(2, "Nontebook");
+		//Comparator<Product> multiFieldComparator = (Product p1, Product p2) -> {
+		//	int result = p1.id - p2.id;
+		//	if(result != 0) return result;
+		//	return p1.name.compareTo(p2.name);
+		//};
 
-		List<Product> products = Arrays.asList(rubber, pen, noteBook);
+		Comparator<Product> multiFieldComparator =  
+			Comparator.<Product>comparingInt(p -> p.id)
+			.thenComparing( p -> p.name);
+
+		Product pen = new Product(1, "Pen");
+		Product rubber = new Product(3, "Rubber");
+		Product fountainPen = new Product(1, "Fountain Pen");
+		Product cup = new Product(2, "Cup");
+
+		List<Product> products = Arrays.asList(rubber, pen, cup, fountainPen);
 		System.out.println(products);
 		
 		System.out.println("Order using the comparable interface:");
@@ -25,5 +36,11 @@ public class ComparatorExamples {
 		System.out.println("Order using the comparator interface:");
 		Collections.sort(products, byId);
                 System.out.println(products);
+
+		System.out.println("Order using the multiproperty interface:");
+                Collections.sort(products, multiFieldComparator);
+                System.out.println(products);
 	}
 }
+
+
